deb3023041c936f35893078f4668d6ae9d66b7ca
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-13 21:14:33 +0000

    initial testing with api server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function App() {
+  const [merchants, setMerchants] = useState(false);
+  useEffect(() => {
+    getMerchant();
+  }, []);
+  function getMerchant() {
+    fetch("http://localhost:3001")
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        setMerchants(data);
+      });
   }
+  function createMerchant() {
+    let name = prompt("Enter merchant name");
+    let email = prompt("Enter merchant email");
+    fetch("http://localhost:3001/merchants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        alert(data);
+        getMerchant();
+      });
+  }
+  function deleteMerchant() {
+    let id = prompt("Enter merchant id");
+    fetch(`http://localhost:3001/merchants/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        alert(data);
+        getMerchant();
+      });
+  }
+  return (
+    <div>
+      {merchants ? merchants : "There is no merchant data available"}
+      <br />
+      <button onClick={createMerchant}>Add merchant</button>
+      <br />
+      <button onClick={deleteMerchant}>Delete merchant</button>
+    </div>
+  );
 }
-
 export default App;
