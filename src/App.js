16cc03fb926eececbcfa1ec99d9dcd930b270050
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-15 00:28:04 +0000

    initial chat widget setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React, { useState, useEffect } from "react";
+import { Widget, addResponseMessage } from "react-chat-widget";
+
+import "react-chat-widget/lib/styles.css";
+
+import logo from "./logo.svg";
 
 function App() {
-  const [merchants, setMerchants] = useState(false);
+  useEffect(
+    () => addResponseMessage("Welcome to the chat! How can we help you?"),
+    []
+  );
+
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    // addResponseMessage("response text");
+    //send ajax request to createNewMessage
+  };
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     console.log("checking");
+  //     //make ajax request from server
+  //     addResponseMessage("response text");
+  //   }, 1000);
+  // });
+
+  const [users, setUsers] = useState(false);
   useEffect(() => {
-    getMerchant();
+    getUsers();
   }, []);
-  function getMerchant() {
+
+  function getUsers() {
     fetch("http://localhost:3001")
       .then((response) => {
         return response.text();
       })
       .then((data) => {
-        setMerchants(data);
+        setUsers(data);
       });
   }
-  function createMerchant() {
-    let name = prompt("Enter merchant name");
-    let email = prompt("Enter merchant email");
-    fetch("http://localhost:3001/merchants", {
+  function createUser() {
+    let name = prompt("Enter user name");
+    let email = prompt("Enter user email");
+    fetch("http://localhost:3001/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,12 +55,12 @@ function App() {
       })
       .then((data) => {
         alert(data);
-        getMerchant();
+        getUsers();
       });
   }
-  function deleteMerchant() {
-    let id = prompt("Enter merchant id");
-    fetch(`http://localhost:3001/merchants/${id}`, {
+  function deleteUser() {
+    let id = prompt("Enter users id");
+    fetch(`http://localhost:3001/users/${id}`, {
       method: "DELETE",
     })
       .then((response) => {
@@ -42,16 +68,22 @@ function App() {
       })
       .then((data) => {
         alert(data);
-        getMerchant();
+        getUsers();
       });
   }
   return (
-    <div>
-      {merchants ? merchants : "There is no merchant data available"}
+    <div className="App">
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        title="Chat with a Mentor"
+        subtitle="Mentor name goes here"
+      />
+      {users ? users : "There is no user data available"}
       <br />
-      <button onClick={createMerchant}>Add merchant</button>
+      <button onClick={createUser}>Add User</button>
       <br />
-      <button onClick={deleteMerchant}>Delete merchant</button>
+      <button onClick={deleteUser}>Delete User</button>
     </div>
   );
 }
