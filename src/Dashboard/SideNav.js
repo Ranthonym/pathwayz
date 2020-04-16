16bdd910ee00038be2780f8913e8ee204be0d60e
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-04-16 02:50:52 +0000

    hid quiz from dashboard

--- src/Dashboard/SideNav.js
+++ src/Dashboard/SideNav.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+// import useVisualMode from "../hooks/useVisualMode";
+import QuizApp from "../components/QuizApp";
 import {
   Navbar,
   NavbarToggler,
@@ -14,18 +15,29 @@ import {
   DropdownItem,
   Collapse,
 } from "shards-react";
-
+// const { mode, transition } = useVisualMode(SHOW);
+// const SHOW = "SHOW";
 export default class DashNav extends React.Component {
   constructor(props) {
     super(props);
 
+    this.startQuiz = this.startQuiz.bind(this);
     this.toggleDropdown = this.toggleDropdown.bind(this);
     this.toggleNavbar = this.toggleNavbar.bind(this);
 
     this.state = {
+      quiz: false,
       dropdownOpen: false,
       collapseOpen: false,
     };
+  }
+
+  startQuiz() {
+    console.log("clicked career quiz");
+    this.setState({
+      ...this.state,
+      ...{ quiz: !this.state.quiz },
+    });
   }
 
   toggleDropdown() {
@@ -46,37 +58,54 @@ export default class DashNav extends React.Component {
     });
   }
 
+  // takeAssessment() {
+  //   transition(SHOW);
+  // }
   render() {
+    const isTakingQuiz = this.state.quiz;
+    let quiz;
+    if (isTakingQuiz) {
+      quiz = <QuizApp totalQuestions={5} />;
+    } else {
+      quiz = <div>nothing was selected</div>;
+    }
     return (
-      <Navbar type="dark" theme="info" expand="md">
-        <NavbarBrand href="#">PathFinder</NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar} />
+      <div>
+        {/* {mode === SHOW && <QuizApp totalQuestions={5} />} */}
+        <Navbar type="dark" theme="info" expand="md">
+          <NavbarBrand href="#">PathFinder</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} />
 
-        <Collapse open={this.state.collapseOpen} navbar>
-          <Nav type="dark" navbar className="ml-auto" expand="md">
-            <NavItem>
-              <Button theme="light">Chat with a mentor!</Button>
-            </NavItem>
-            <NavItem>
-              <NavLink>About</NavLink>
-            </NavItem>
-            <Dropdown
-              nav={true}
-              open={this.state.dropdownOpen}
-              toggle={this.toggleDropdown}
-            >
-              <DropdownToggle nav caret>
-                <i className="fa fa-fw fa-user"></i>
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>My Dashboard</DropdownItem>
-                <DropdownItem>Career Quiz</DropdownItem>
-                <DropdownItem>Logout</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
+          <Collapse open={this.state.collapseOpen} navbar>
+            <Nav type="dark" navbar className="ml-auto" expand="md">
+              <NavItem>
+                <Button theme="light">Chat with a mentor!</Button>
+              </NavItem>
+              <NavItem>
+                <NavLink>About</NavLink>
+              </NavItem>
+              <Dropdown
+                nav={true}
+                open={this.state.dropdownOpen}
+                toggle={this.toggleDropdown}
+              >
+                <DropdownToggle nav caret>
+                  <i className="fa fa-fw fa-user"></i>
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem onClick={this.startQuiz}>
+                    {" "}
+                    Career Quiz
+                  </DropdownItem>
+                  <DropdownItem> My Dashboard</DropdownItem>
+                  <DropdownItem>Logout</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        {quiz}
+      </div>
     );
   }
 }
