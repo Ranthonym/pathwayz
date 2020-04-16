8412e0eff9e911438dbcbea68f2f8ad6fab55b6f
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-16 01:11:55 +0000

    initial login static page

--- src/Main/MainNav.js
+++ src/Main/MainNav.js
@@ -1,37 +1,23 @@
 import React from "react";
 
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from "shards-react";
-
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
 
 export default function MainNav() {
+  return (
+    <Navbar type="dark" theme="info" expand="md">
+      <NavbarBrand href="#">PathFinder</NavbarBrand>
 
-    return (
-      <Navbar type="dark" theme="info" expand="md">
-        <NavbarBrand href="#">PathFinder</NavbarBrand>
-
-          <Nav type="dark" navbar className="ml-auto" expand="md">
-          <NavItem>
-              <NavLink>
-                Signup
-              </NavLink>
-              </NavItem>
-              <NavItem>
-              <NavLink>
-                Login
-              </NavLink>
-              </NavItem>
-              <NavItem>
-              <NavLink>
-                Help
-              </NavLink>
-              </NavItem>
-          </Nav>
-          </Navbar>
-    );
-  }
+      <Nav type="dark" navbar className="ml-auto" expand="md">
+        <NavItem>
+          <NavLink>Signup</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/login">Login</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>Help</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  );
+}
