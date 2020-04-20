1a1bcce0977fdb472b269122d8de22a97f71e473
Raho <rahojmohamed@gmail.com>
2020-04-20 02:30:41 -0400

    added hover to button on sidenav

--- src/components/resources/Blogs.js
+++ src/components/resources/Blogs.js
@@ -23,6 +23,7 @@ export default class Blogs extends React.Component {
   render() {
   return (
     <div>
+      <h2>Additional Resources</h2>
     <InputGroup className="input">
     <FormInput />
     <Dropdown
@@ -67,12 +68,12 @@ export default class Blogs extends React.Component {
         <Row className="row">
         <Col>
         <Card className="blog-item">
-            <CardImg top src="https://www.usnews.com/dims4/USNEWS/b1b97c0/2147483647/crop/2000x1334%2B0%2B0/resize/970x647/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2Fd1%2Fb7%2Fbdb972e045cfbc6062bd17aff993%2Fstudentdebt.jpg" />
-            <div align="center"><CardHeader><a href="https://www.usnews.com/education/blogs/student-loan-ranger/articles/reasons-to-pay-student-loan-interest-during-school">5 Ways to Self-Motivate When You've Lost Your Drive</a></CardHeader></div>
+            <CardImg top src="https://www.edgeip.com/images/FCK/Image/202003/25-CPAOntario-HowtoSelfMotivate.jpg" />
+            <div align="center"><CardHeader><a href="https://www.schoolfinder.com/Discover/Article/1/5201/5-Ways-to-Self-Motivate-When-You-ve-Lost-Your-Drive">5 Ways to Self-Motivate When You've Lost Your Drive</a></CardHeader></div>
             <CardBody>
-              <p>Classes cancelled and moving online? COVID-19 is no joke, but neither is your education. Find advice to keep on top of your work.</p>
+              <p>Reframe, reassess, and be ready to try again.</p>
             </CardBody>
-            <CardFooter className="blog-footer"><div>By: Logan Bright</div><div>Mar 16, 2020</div></CardFooter>
+            <CardFooter className="blog-footer"><div>By CPA Ontario</div><div>Mar 31, 2020</div></CardFooter>
           </Card>
         </Col>
         </Row>
@@ -80,12 +81,25 @@ export default class Blogs extends React.Component {
         <Row className="row">
         <Col>
         <Card className="blog-item">
-            <CardImg top src="https://www.edgeip.com/images/FCK/Image/202003/00-SFG-HowtoWorkandStudyFromHomeSFSIC.jpg" />
-            <div align="center"><CardHeader><a href="https://www.schoolfinder.com/Discover/Article/1/5187/How-to-Work-and-Study-From-Home">How to Work and Study From Home</a></CardHeader></div>
+            <CardImg top src="https://www.edgeip.com/images/FCK/Image/202002/00-SFG-SmallScholarshipsBigRewardsSFSCSIC.jpg" />
+            <div align="center"><CardHeader><a href="https://www.scholarshipscanada.com/News/9/5155/Small-Scholarships,-Big-Rewards">Small Scholarships, Big Rewards</a></CardHeader></div>
             <CardBody>
               <p>Classes cancelled and moving online? COVID-19 is no joke, but neither is your education. Find advice to keep on top of your work.</p>
             </CardBody>
-            <CardFooter className="blog-footer"><div>By: Logan Bright</div><div>Mar 16, 2020</div></CardFooter>
+            <CardFooter className="blog-footer"><div>By: Scholarship Canada</div><div>Feb 07, 2020</div></CardFooter>
+          </Card>
+        </Col>
+        </Row>
+
+        <Row className="row">
+        <Col>
+        <Card className="blog-item">
+            <CardImg top src="https://www.edgeip.com/images/FCK/Image/202002/00-SFG-SmallScholarshipsBigRewardsSFSCSIC.jpg" />
+            <div align="center"><CardHeader><a href="https://www.scholarshipscanada.com/News/9/5155/Small-Scholarships,-Big-Rewards">Small Scholarships, Big Rewards</a></CardHeader></div>
+            <CardBody>
+              <p>Classes cancelled and moving online? COVID-19 is no joke, but neither is your education. Find advice to keep on top of your work.</p>
+            </CardBody>
+            <CardFooter className="blog-footer"><div>By: Scholarship Canada</div><div>February 07, 2020</div></CardFooter>
           </Card>
         </Col>
         </Row>
