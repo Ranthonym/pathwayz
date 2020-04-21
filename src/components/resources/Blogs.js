598ac31144afbef8f8e8b02b946e29ce32a40ffa
Amino Asmaa Ali <43259449+asmxali@users.noreply.github.com>
2020-04-21 17:46:46 -0400

    Revert "Edit/resources"

--- src/components/resources/Blogs.js
+++ src/components/resources/Blogs.js
@@ -13,7 +13,6 @@ import {
 } from "shards-react";
 import "./Blogs.css";
 import Videos from "./Videos";
-import BlogPost from "./BlogPost";
 
 export default class Blogs extends React.Component {
   constructor(props) {
@@ -47,38 +46,171 @@ export default class Blogs extends React.Component {
     if (this.state.displayBlogs) {
       container = (
         <div>
-          <BlogPost />
-          <BlogPost />
+          <div id="blog-container">
+            <Row className="row">
+              <Col>
+                <Card id="blog-card" className="blog-item">
+                  <CardImg
+                    top
+                    src="https://www.online-bachelor-degrees.com/wp-content/uploads/2018/05/finance-management-tools.jpg"
+                  ></CardImg>
+                  <div align="center">
+                    <CardHeader>
+                      <a href="https://uwaterloo.ca/beyond-ideas/stories/financing/ultimate-guide-financing-your-education">
+                        Ultimate guide: Financing your education
+                      </a>
+                    </CardHeader>
+                  </div>
+                  <CardBody>
+                    <p>
+                      Getting scholarships and bursaries can make a big
+                      difference for your funds.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="blog-footer">
+                    <div>By: University of Waterloo</div>
+                    <div>February 28, 2020</div>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+
+            <Row className="row">
+              <Col>
+                <Card id="blog-card" className="blog-item">
+                  <CardImg
+                    top
+                    src="https://www.edgeip.com/images/FCK/Image/202003/00-SFG-HowtoWorkandStudyFromHomeSFSIC.jpg"
+                  />
+                  <div align="center">
+                    <CardHeader>
+                      <a href="https://www.schoolfinder.com/Discover/Article/1/5187/How-to-Work-and-Study-From-Home">
+                        How to Work and Study From Home
+                      </a>
+                    </CardHeader>
+                  </div>
+                  <CardBody>
+                    <p>
+                      Classes cancelled and moving online? COVID-19 is no joke,
+                      but neither is your education. Find advice to keep on top
+                      of your work.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="blog-footer">
+                    <div>By: Logan Bright</div>
+                    <div>March 16, 2020</div>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+
+            <Row className="row">
+              <Col>
+                <Card id="blog-card" className="blog-item">
+                  <CardImg
+                    top
+                    src="https://www.edgeip.com/images/FCK/Image/202003/25-CPAOntario-HowtoSelfMotivate.jpg"
+                  />
+                  <div align="center">
+                    <CardHeader>
+                      <a href="https://www.schoolfinder.com/Discover/Article/1/5201/5-Ways-to-Self-Motivate-When-You-ve-Lost-Your-Drive">
+                        5 Ways to Self-Motivate When You've Lost Your Drive
+                      </a>
+                    </CardHeader>
+                  </div>
+                  <CardBody>
+                    <p>Reframe, reassess, and be ready to try again.</p>
+                  </CardBody>
+                  <CardFooter className="blog-footer">
+                    <div>By: CPA Ontario</div>
+                    <div>March 31, 2020</div>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+          <div id="blog-container">
+            <Row className="row">
+              <Col>
+                <Card id="blog-card" className="blog-item">
+                  <CardImg
+                    top
+                    src="https://www.edgeip.com/images/FCK/Image/202002/00-SFG-SmallScholarshipsBigRewardsSFSCSIC.jpg"
+                  />
+                  <div align="center">
+                    <CardHeader>
+                      <a href="https://www.scholarshipscanada.com/News/9/5155/Small-Scholarships,-Big-Rewards">
+                        Small Scholarships, Big Rewards
+                      </a>
+                    </CardHeader>
+                  </div>
+                  <CardBody>
+                    <p>
+                      Classes cancelled and moving online? COVID-19 is no joke,
+                      but neither is your education. Find advice to keep on top
+                      of your work.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="blog-footer">
+                    <div>By: Scholarship Canada</div>
+                    <div>February 07, 2020</div>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+
+            <Row className="row">
+              <Col>
+                <Card id="blog-card" className="blog-item">
+                  <CardImg
+                    top
+                    src="https://www.edgeip.com/images/FCK/Image/202002/00-SFG-TopScholarshipApplicationKeywords.jpg"
+                  />
+                  <div align="center">
+                    <CardHeader>
+                      <a href="https://www.scholarshipscanada.com/News/9/5152/7-Keywords-to-Include-When-Applying-for-Scholarships">
+                        7 Keywords to Include When Applying for Scholarships
+                      </a>
+                    </CardHeader>
+                  </div>
+                  <CardBody>
+                    <p>
+                      Classes cancelled and moving online? COVID-19 is no joke,
+                      but neither is your education. Find advice to keep on top
+                      of your work.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="blog-footer">
+                    <div>By: Scholarship Canada</div>
+                    <div>February 07, 2020</div>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
       );
     }
     if (this.state.displayVideos) {
-      container = (
-        <div>
-          {" "}
-          <Videos />
-          <Videos />
-        </div>
-      );
+      container = <Videos />;
     }
     return (
       <div>
-
-        <InputGroup id="resources-input" className="input">
-          <FormInput placeholder="Search resources" />
-          <Dropdown
-            addonType="append"
-            open={this.state.open}
-            toggle={this.toggle}
-          >
-            <DropdownToggle theme="info" caret></DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem onClick={this._onBlogClick}>Blogs</DropdownItem>
-              <DropdownItem onClick={this._onVideoClick}>Videos</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </InputGroup>
-
+        <h3>
+          <strong>Additional Resources</strong>
+        </h3>
+        <div align="center">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbItem active onClick={this._onBlogClick}>
+            <a href="#">Blogs</a>
+            </BreadcrumbItem>
+          </BreadcrumbItem>
+          <BreadcrumbItem active onClick={this._onVideoClick}>
+             <a href="#">Videos</a>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        </div>
         <Container className="container">{container}</Container>
       </div>
     );
