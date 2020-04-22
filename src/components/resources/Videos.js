d40716ee6f93c947b51ab89c1891857dde5395bf
Raho <rahojmohamed@gmail.com>
2020-04-21 20:42:54 -0400

    fixed blog.js

--- src/components/resources/Videos.js
+++ src/components/resources/Videos.js
@@ -76,3 +76,102 @@ export default function Videos() {
             </Card>
           </Col>
         </Row>
+
+        <Row className="row">
+          <Col>
+            <Card id="blog-card" className="blog-item">
+              <iframe
+                className="video"
+                src="https://www.youtube.com/embed/kMeDuvprqsU"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <div align="center">
+                <CardHeader>
+                  <a href="https://www.youtube.com/embed/kMeDuvprqsU">
+                    Our Top 5 Tips for Choosing the Right University [VIDEO]
+                  </a>
+                </CardHeader>
+              </div>
+              <CardBody>
+                <p>
+                  Western University grads share what they wish they knew going
+                  into first year.
+                </p>
+              </CardBody>
+              <CardFooter className="blog-footer">
+                <div>By: Western University</div>
+                <div>September 27, 2019</div>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div id="blog-container">
+        <Row className="row">
+          <Col>
+            <Card id="blog-card" className="blog-item">
+              <iframe
+                className="video"
+                src="https://www.youtube.com/embed/1Zw8Bfe9lV8"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <div align="center">
+                <CardHeader>
+                  <a href="https://www.youtube.com/embed/1Zw8Bfe9lV8">
+                    Life After High School: Tips for Choosing the Right School
+                    [VIDEO]
+                  </a>
+                </CardHeader>
+              </div>
+              <CardBody>
+                <p>
+                  ScholarshipsCanada asked five recent grads to share their
+                  story. Here are their tips for choosing the right school!
+                </p>
+              </CardBody>
+              <CardFooter className="blog-footer">
+                <div>By: Scholarship Canada</div>
+                <div>December 12, 2017</div>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row className="row">
+          <Col>
+            <Card id="blog-card" className="blog-item">
+              <iframe
+                className="video"
+                src="https://www.youtube.com/embed/bGmWFWWEaj8"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <div align="center">
+                <CardHeader>
+                  <a href="https://www.youtube.com/embed/bGmWFWWEaj8">
+                    Deciding What to Study [VIDEO]
+                  </a>
+                </CardHeader>
+              </div>
+              <CardBody>
+                <p>
+                  We share our best advice for choosing your programs in this
+                  short video.
+                </p>
+              </CardBody>
+              <CardFooter className="blog-footer">
+                <div>By: Vancouver Island University</div>
+                <div>January 14, 2020</div>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
