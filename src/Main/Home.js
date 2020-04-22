8d25abd096217f5f36f7ad00a3be63a100f2c6d3
Raho <rahojmohamed@gmail.com>
2020-04-22 04:43:59 -0400

    made changes to homepage

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "shards-react";
-import "./Home.css"
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+} from "shards-react";
 
-import Pathwayz from "./Pathwayz.jpg"
+
+import "./Home.css";
+import Testimonial from "./Testimonial"
+
+import Pathwayz from "./Pathwayz.jpg";
 
 export default function Home() {
-const imageUrl = Pathwayz;
+  const imageUrl = Pathwayz;
   return (
     <div>
       <Navbar type="dark" theme="info" expand="md">
@@ -24,11 +34,40 @@ const imageUrl = Pathwayz;
           </NavItem>
         </Nav>
       </Navbar>
-         <div className="Home" style={{backgroundImage: `url(${imageUrl})` }}>
-         <div className="Home-content">
-             <p className="slogan">finding the right program and career just got easier than ever.</p>
-         </div>
+      <div className="Home1" style={{ backgroundImage: `url(${imageUrl})` }}>
+        <div className="Home-content1">
+          <p className="slogan">
+            finding the right program and career just got easier than ever.
+          </p>
+        </div>
       </div>
+
+      <div className="Home2">
+        <div className="Home-content2">
+
+          <div className="home-button">
+          <Button theme="warning" size="lg">
+            Take our Career Assessment Today!
+          </Button>
+          </div>
+
+          <div className="testimonial">
+            <Testimonial/>
+          </div>
+
+          <div className="mentors">
+          <p className="mentor-title">Our Mentors!</p>
+          <img className="avatar-size" alt="Remy Sharp" src="https://tinyfac.es/data/avatars/7D3FA6C0-83C8-4834-B432-6C65ED4FD4C3-500w.jpeg" />
+          <img className="avatar-size" alt="Travis Howard" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMjA0ODk1OF5BMl5BanBnXkFtZTcwMTA4ODM3OQ@@._V1_UY256_CR5,0,172,256_AL_.jpg" />
+          <img className="avatar-size" alt="Cindy Baker"  src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjExOTY3NzExM15BMl5BanBnXkFtZTgwOTg1OTAzMTE@._V1_UX172_CR0,0,172,256_AL_.jpg" />
+          </div>
+          <div className="mentors">
+          <img className="avatar-size" alt="Cindy Baker"  src="https://randomuser.me/api/portraits/men/81.jpg" />
+          <img className="avatar-size" alt="Cindy Baker"  src="https://randomuser.me/api/portraits/women/86.jpg" />
+          <img className="avatar-size" alt="Cindy Baker"  src="https://pbs.twimg.com/profile_images/950813895291297794/IAUhzxPw.jpg" />
+          </div>
       </div>
+    </div>
+    </div>
   );
 }
---
+++ src/Main/Testimonial.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function Testimonial() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Testimonial
+        </Typography>
+        <Typography variant="h5" component="h2">
+          "This was the best decision I made."
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+         -Sarah Wiseman(12th grade student)
+        </Typography>
+        </CardContent>
+    </Card>
+  );
+}
