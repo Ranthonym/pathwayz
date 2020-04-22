54289778e727d8ebe2b26a70f339be19c2c90e05
Raho <rahojmohamed@gmail.com>
2020-04-22 06:00:26 -0400

    homepage complete

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -47,7 +47,7 @@ export default function Home() {
 
           <div className="home-button">
           <Button theme="warning" size="lg">
-            Take our Career Assessment Today!
+            <div><strong>Take our Career Assessment Today!</strong></div>
           </Button>
           </div>
 
@@ -56,15 +56,19 @@ export default function Home() {
           </div>
 
           <div className="mentors">
-          <p className="mentor-title">Our Mentors!</p>
+          <h3 className="mentor-title">Our Mentors</h3>
           <img className="avatar-size" alt="Remy Sharp" src="https://tinyfac.es/data/avatars/7D3FA6C0-83C8-4834-B432-6C65ED4FD4C3-500w.jpeg" />
           <img className="avatar-size" alt="Travis Howard" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMjA0ODk1OF5BMl5BanBnXkFtZTcwMTA4ODM3OQ@@._V1_UY256_CR5,0,172,256_AL_.jpg" />
           <img className="avatar-size" alt="Cindy Baker"  src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjExOTY3NzExM15BMl5BanBnXkFtZTgwOTg1OTAzMTE@._V1_UX172_CR0,0,172,256_AL_.jpg" />
+          <img className="avatar-size" alt="Cindy Baker"  src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMjM1MzgxMl5BMl5BanBnXkFtZTcwNDI0NDE5NQ@@._V1_UY256_CR4,0,172,256_AL_.jpg" />
+
           </div>
           <div className="mentors">
           <img className="avatar-size" alt="Cindy Baker"  src="https://randomuser.me/api/portraits/men/81.jpg" />
           <img className="avatar-size" alt="Cindy Baker"  src="https://randomuser.me/api/portraits/women/86.jpg" />
           <img className="avatar-size" alt="Cindy Baker"  src="https://pbs.twimg.com/profile_images/950813895291297794/IAUhzxPw.jpg" />
+          <img className="avatar-size" alt="Cindy Baker"  src="https://randomuser.me/api/portraits/women/65.jpg" />
+          
           </div>
       </div>
     </div>
--- src/Main/Testimonial.js
+++ src/Main/Testimonial.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
   },
   title: {
     fontSize: 14,
@@ -24,18 +18,16 @@ const useStyles = makeStyles({
 export default function Testimonial() {
   const classes = useStyles();
   return (
-    <Card className={classes.root}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           Testimonial
         </Typography>
         <Typography variant="h5" component="h2">
-          "This was the best decision I made."
+        "PathWayz provided me with convenient online service, which helped me understand myself better. I am now confident in my decision to attend college."
         </Typography>
         <Typography className={classes.pos} color="textSecondary">
-         -Sarah Wiseman(12th grade student)
+         -Sarah Wiseman
         </Typography>
         </CardContent>
-    </Card>
   );
 }
